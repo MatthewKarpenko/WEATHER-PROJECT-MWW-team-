0c8c43c33a720ee62690da1710786eb7ecaee912
Matvii Karpenko <mtw.karp@gmail.com>
2019-04-08 23:54:10 +0200

    working on server

--- server.js
+++ server.js
@@ -1,56 +1,16 @@
-var
-    http = require('http'), //helps with http methods
-    path = require('path'), //helps with file paths
-    fs = require('fs'); //helps with file system tasks
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
 
-//a helper function to handle HTTP requests
-function requestHandler(req, res) {
-    var
-        content = '',
-        fileName = path.basename(req.url), //the file that was requested
-        localFolder = __dirname + '/public/'; //where our public files are located
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.join(__dirname, './public')));
 
-    //NOTE: __dirname returns the root folder that
-    //this javascript file is in.
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+    console.log('sadsdasdasd');
+    console.log(path.join(__dirname, './public', 'index.html'));
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
 
-    if (fileName === 'index.html') { //if index.html was requested...
-        content = localFolder + fileName; //setup the file name to be returned
-
-        //reads the file referenced by 'content'
-        //and then calls the anonymous function we pass in
-        fs.readFile(content, function (err, contents) {
-            //if the fileRead was successful...
-            if (!err) {
-                //send the contents of index.html
-                //and then close the request
-                res.end(contents);
-            } else {
-                //otherwise, let us inspect the eror
-                //in the console
-                console.dir(err);
-            };
-        });
-    } else {
-        console.log(__dirname + req.url);
-        //reads the file referenced by 'content'
-        //and then calls the anonymous function we pass in
-        fs.readFile(__dirname + '/public/' + req.url, function (err, contents) {
-            //if the fileRead was successful...
-            if (!err) {
-                //send the contents of index.html
-                //and then close the request
-                res.end(contents);
-            } else {
-                //otherwise, let us inspect the eror
-                //in the console
-                console.dir(err);
-            };
-        });
-    };
-};
-
-//step 2) create the server
-http.createServer(requestHandler)
-
-    //step 3) listen for an HTTP request on port 3000
-    .listen(3000);
+app.listen(port);

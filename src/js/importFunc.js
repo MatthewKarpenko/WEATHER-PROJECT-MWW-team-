d71e5bab5f1b325cc56151997161cad43f14e612
MatthewKarpenko <mtw.karp@gmail.com>
2019-04-09 15:46:34 +0200

    geolocation changes

--- src/js/importFunc.js
+++ src/js/importFunc.js
@@ -2,6 +2,67 @@
 import {fiveDays} from './maps.js';
 import {showMap} from './map.js';
 
+function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+  var geocoder =  new google.maps.Geocoder();;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    console.log(latlng)
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map2'), mapOptions);
+  }
+
+  function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+ document.getElementById('mapButton').addEventListener('click', codeAddress)
+  window.onload = initialize
 
 let findButton = document.querySelector('#getCityName');
 let mainInput = document.querySelector('.search__input');
@@ -17,7 +78,7 @@ let place = {
 //request to check weather
 var getJSON = function (url, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    xhr.open('POST', url, true);
     xhr.responseType = 'json';
     xhr.onload = function () {
         var status = xhr.status;

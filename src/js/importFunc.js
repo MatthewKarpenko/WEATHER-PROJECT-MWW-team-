bd6fc47bf07cdc53a8f253dd2b240431fafaba00
Matvii Karpenko <mtw.karp@gmail.com>
2019-04-09 16:34:45 +0200

    new geolocation functionality

--- src/js/importFunc.js
+++ src/js/importFunc.js
@@ -2,67 +2,7 @@
 import {fiveDays} from './maps.js';
 import {showMap} from './map.js';
 
-function geoFindMe() {
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if (!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  
-  }
-  
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
-
-  var geocoder =  new google.maps.Geocoder();;
-  var map;
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    console.log(latlng)
-    var mapOptions = {
-      zoom: 8,
-      center: latlng
-    }
-    map = new google.maps.Map(document.getElementById('map2'), mapOptions);
-  }
-
-  function codeAddress() {
-    var address = document.getElementById('address').value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
- document.getElementById('mapButton').addEventListener('click', codeAddress)
-  window.onload = initialize
 
 let findButton = document.querySelector('#getCityName');
 let mainInput = document.querySelector('.search__input');
@@ -94,12 +34,13 @@ var getJSON = function (url, callback) {
 
 //Take current location of user
 function takeLocationByIp() {
-    getJSON("http://ip-api.com/json/",
+    getJSON("http://api.ipstack.com/156.17.137.73?access_key=ff3d09f2ea5abe10103f62333c221102",
         function (json) {
+          
             place.cityName = json.city;
-            place.countryName = json.country;
-            place.x = json.lat;
-            place.y =json.lon;
+            place.countryName = json.country_name;
+            place.x = json.latitude;
+            place.y =json.longitude;
             showMap(place.x, place.y);
             fiveDays(place.cityName,check);
             check ++;
